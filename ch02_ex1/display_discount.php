839772db8574d655f3179bc3ca00eb3babfc09ba
lcromrell <lcromrell@MamaMorada>
2015-09-30 13:43:24 -0600

    Changes for Chapter 2 Exercise 1

--- ch02_ex1/display_discount.php
+++ ch02_ex1/display_discount.php
@@ -1,3 +1,19 @@
+<?php
+    // get the data from the form
+    $product_description = $_POST['product_description'];
+    $list_price = $_POST['list_price'];
+    $discount_percent = $_POST['discount_percent'];
+    
+    // calculate the discout and discounted price
+    $discount = $list_price * $discount_percent * .01;
+    $discount_price = $list_price - $discount;
+    
+    // apply currency formatting to the dollar and percent amounts
+    $list_price_formatted = "$".number_format($list_price, 2);
+    $discount_percent_formatted = $discount_percent."%";
+    $discount_formatted = "$".number_format($discount, 2);
+    $discount_price_formatted = "$".number_format($discount_price, 2);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,8 +22,8 @@
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 <body>
-    <div id="content">
-        <h1>This page is under construction</h1>
+     <div id="content">
+        <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
         <span><?php echo $product_description; ?></span><br />
